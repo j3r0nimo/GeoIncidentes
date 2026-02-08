ff4d3dda1c7c57eee2bc496c06bb51005e666577
shimma <jimemartinezarana@hotmail.com>
2026-02-08 14:14:42 +0000

    app y server, archivos env, git ignore y configuracion

---
+++ src/server.js
@@ -0,0 +1,25 @@
+/* connects DB
+ * listens (runtime only)
+ * This file is NEVER imported by tests
+ */
+
+import app from "./app.js";
+import connectDB from "../../origen/src/db/db.js"; // Conexión a MongoDB
+import { PORT } from "../../origen/config/env.js"; // Puerto backend
+
+// inicio del servidor
+(async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Environment: ${process.env.NODE_ENV}`);
+      console.log(
+        `Skynet is fully operational at http://localhost:${PORT}/api/incidentes`,
+      );
+    });
+  } catch (error) {
+    console.error("Skynet no permite la conexión...", error);
+    process.exit(1);
+  }
+})();
