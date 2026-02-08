ac7f146bd690ca82bee1f782ef2fa6bddddbbfa1
j3r0nimo <jerobalort@gmail.com>
2026-02-08 12:33:21 -0300

    arreglo de icono sin imagen

--- src/pages/administracion.jsx
+++ src/pages/administracion.jsx
@@ -2,7 +2,20 @@ import { useState, useEffect } from 'react';
 import { Form, Button, Row, Col, Table, Container } from "react-bootstrap";
 import { useNavigate } from 'react-router-dom';
 import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 function ManejadorClics({ alHacerClic, posicionActual }) {
     useMapEvents({
         click(e) {
